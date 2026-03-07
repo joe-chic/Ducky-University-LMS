f38184da085d545c0daca5630604085b016e729f
Jonathan Humphreys <jehumphreys09@gmail.com>
2026-03-07 02:01:18 -0600

    Adding user_accounts_db definition

--- db/user_accounts_db/user_accounts_db.sql
+++ db/user_accounts_db/user_accounts_db.sql
@@ -1,8 +1,47 @@
 CREATE DATABASE user_accounts_db;
 \c user_accounts_db;
 
-CREATE TABLE users(
-	user_id SERIAL PRIMARY KEY,
-	campus_id UNIQUE,
+-- Data type definitions
+CREATE DOMAIN sys_email AS VARCHAR(255)
+CHECK(VALUE ~* '^(?!.*\.\..*)(?!\..*)[A-Za-z0-9\.\_\%\+\-]+@(?:ducky\.edu|udem\.edu)$');
 
+CREATE TYPE account_state AS ENUM('active', 'blocked', 'disabled');
+
+-- Table definitions
+CREATE TABLE users(
+	user_id BIGSERIAL PRIMARY KEY,
+	campus_id SERIAL NOT NULL UNIQUE,
+	first_name VARCHAR(100) NOT NULL,
+	middle_name VARCHAR(100),
+	father_lastname VARCHAR(100) NOT NULL,
+	mother_lastname VARCHAR(100),
+	user_email sys_email NOT NULL,
+	user_password VARCHAR(255) NOT NULL,
+	user_state account_state NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),
+	created_by BIGINT NOT NULL,
+	latest_modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	latest_modified_by BIGINT NOT NULL,
+	latest_disabled_at TIMESTAMPTZ,
+	latest_disabled_by BIGINT
+);
+
+CREATE TABLE roles(
+	role_id SMALLSERIAL PRIMARY KEY,
+	role_name VARCHAR(50) NOT NULL,
+	role_description VARCHAR(255)
+);
+
+CREATE TABLE permissions(
+	permission_id SERIAL PRIMARY KEY,
+	permission_name VARCHAR(50) NOT NULL,
+	permission_description VARCHAR(255)
+);
+
+CREATE TABLE roles_permissions(
+	role_id INT,
+	permission_id INT,
+	PRIMARY KEY(role_id, permission_id),
+	CONSTRAINT role_fk FOREIGN KEY(role_id) REFERENCES roles(role_id),
+	CONSTRAINT permission_fk FOREIGN KEY(permission_id) REFERENCES permissions(permission_id)
 );
